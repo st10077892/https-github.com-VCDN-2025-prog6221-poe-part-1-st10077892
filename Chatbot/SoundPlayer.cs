4b464fbdf4d7172f4dbc84cc35753c181bbecb7c
Nonjabulo Mathenjwa <ST10077892@vcconnect.edu.za>
2025-04-22 19:42:53 +0200

    updated code

--- Chatbot/SoundPlayer.cs
+++ Chatbot/SoundPlayer.cs
@@ -11,20 +11,25 @@ using System.Media;
 //Link: https://learn.microsoft.com/en-us/azure/bot-service/bot-service-quickstart-create-bot?view=azure-bot-service-4.0&tabs=csharp%2Cvs
 //Date accessed: 15 April 2025
 
-//Name: 
-//Author:
-//Link:
-//Date accessed:
+//Name: C# Inheritance
+//Author: W3Schools
+//Link:https://www.w3schools.com/cs/cs_inheritance.php
+//Date accessed: 15 April 2025
 
-//Name: 
-//Author:
-//Link:
-//Date accessed:
+//Name: Types of Cyber Attacks
+//Author: Check Point
+//Link: https://www.checkpoint.com/cyber-hub/cyber-security/what-is-cyber-attack/types-of-cyber-attacks/
+//Date accessed: 15 April 2025
 
-//Name: 
-//Author:
-//Link:
-//Date accessed:
+//Name: SoundPlayer Class
+//Author: Microsoft
+//Link: https://learn.microsoft.com/en-us/dotnet/api/system.media.soundplayer?view=windowsdesktop-9.0
+//Date accessed: 15 April 2025
+
+//Name: Validating Input
+//Author: Makers Institute
+//Link: https://makersinstitute.gitbooks.io/c-basics/content/validating-input.html
+//Date accessed: 15 April 2025
 
 namespace Chatbot
 {
